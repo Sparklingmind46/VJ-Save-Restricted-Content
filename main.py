ae4264704fb245ad2ac826a90f62bf219f087b4e
Sparklingmind46 <sparklingmind42@gmail.com>
2024-06-10 12:42:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
+	bot.send_message(message.chat.id, f"**__👋 Hii** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/Amit_4626")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
@@ -75,7 +75,7 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 	if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
 
 		if acc is None:
-			bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
+			bot.send_message(message.chat.id,f"**String Session is not Set, Humse na ho paayega maalik**", reply_to_message_id=message.id)
 			return
 
 		try:
